eaa7a2611d35f60d4bc4fdc18896f5c3c5dccb10
kite_U <109857975+youyeon11@users.noreply.github.com>
2024-10-19 12:11:53 +0900

    [fix]

--- src/main/java/com/tave/gangnam/week5/assignment/repository/EmployeeRepository.java
+++ src/main/java/com/tave/gangnam/week5/assignment/repository/EmployeeRepository.java
@@ -3,5 +3,8 @@ package com.tave.gangnam.week5.assignment.repository;
 import com.tave.gangnam.week5.assignment.domain.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Optional<Employee> findByEmployeeNo(String employeeNo);
 }
--- src/main/java/com/tave/gangnam/week5/assignment/type/ErrorCode.java
+++ src/main/java/com/tave/gangnam/week5/assignment/type/ErrorCode.java
@@ -8,7 +8,9 @@ import lombok.Getter;
 public enum ErrorCode {
     USER_ALREADY_EXISTS("이미 존재하는 유저입니다."),
     USER_NOT_FOUND("존재하지 않는 유저입니다."),
-    PASSWORD_UNMATCHED("비밀번호가 일치하지 않습니다");
+    PASSWORD_UNMATCHED("비밀번호가 일치하지 않습니다"),
+    POSTS_NOT_FOUND("해당 게시글을 찾을 수 없습니다."),
+    WRONG_PASSWORD("비밀번호가 틀렸습니다.");
 
 
     private final String description;
